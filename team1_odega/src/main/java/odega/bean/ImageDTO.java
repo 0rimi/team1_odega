d258151cbffa1b016e091f79e58203c5066cbc39
y <y@192.168.219.150>
2023-10-23 14:24:32 +0900

    postView 완료

--- team1_odega/src/main/java/odega/bean/ImageDTO.java
+++ team1_odega/src/main/java/odega/bean/ImageDTO.java
@@ -2,4 +2,41 @@ package odega.bean;
 
 public class ImageDTO {
 
+	//field
+	private int num;
+	private String img_url;
+	private int posts_num;
+	private int post_image_num;
+	
+	//g&s
+	public int getNum() {
+		return num;
+	}
+	public String getImg_url() {
+		return img_url;
+	}
+	public int getPosts_num() {
+		return posts_num;
+	}
+	public int getPost_image_num() {
+		return post_image_num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
+	}
+	public void setPosts_num(int posts_num) {
+		this.posts_num = posts_num;
+	}
+	public void setPost_image_num(int post_image_num) {
+		this.post_image_num = post_image_num;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "ImageDTO [num=" + num + ", img_url=" + img_url + ", posts_num=" + posts_num + ", post_image_num=" + post_image_num + "]";
+	}
 }
